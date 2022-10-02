0ae440ca15526156e506f2eea1400dba06d650ad
rachelcherry <102711433+rachelcherry@users.noreply.github.com>
2022-10-02 10:32:40 -0400

    migrate from glitch

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# spotLight
-In spotLight, we are creating a feature in Spotify to connect users to new/underground artists. This project will use the Spotify API, and collect data that tells us what what songs they currently enjoy, and match them with artists that they may enjoy. 
+# Hello users!
+
+Hello! This is a interactive website that helps users find new music and new artists!
+
+## What's in this project?
+
+← `README.md`: That's this file, where you can tell people what your cool website does and how you built it.
+
+← `index.html`: This is the main web page for your site. The HTML defines the structure and content of the page using _elements_. You'll see references in the HTML to the JS and CSS files. Try clicking the image in the center of the page!
+
+← `style.css`: CSS files add styling rules to your content. The CSS applies styles to the elements in your HTML page. The style rules also make the image move when you click it.
+
+← `script.js`: The code in the JavaScript file runs when the page loads, and when the visitor clicks the button you can add below.
+
+
+
---
+++ script.js
@@ -0,0 +1,73 @@
+const btn = btn
+
+
+//1=no 2=yes
+//import api data?
+//const hashset = inserting names of artists that we've seen
+//const array = array of api artists. array size = 10
+//while (i<10)
+//if (!set.contains(artist_name))
+//array[i]=artist_name
+//set.add(artist-name)
+//i++
+// Detect clicks on the button
+
+if (btn) {
+  btn.onclick = function() {
+    console.log("clicked")
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+    //IDK WHAT THIS DOES T_T
+  }
+}
+else if (btn2) {
+  btn2.onclick = function() {
+    console.log("clicked")
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn2.classList.toggle("dipped");
+    //IDK WHAT THIS DOES T_T
+  }
+}
+
+
+// This is a single line JS comment
+/*
+This is a comment that can span multiple lines 
+- use comments to make your own notes!
+*/console.log(btn)
+
+//1=no 2=yes
+//import api data?
+//const hashset = inserting names of artists that we've seen
+//const array = array of api artists. array size = 10
+//while (i<10)
+//if (!set.contains(artist_name))
+//array[i]=artist_name
+//set.add(artist-name)
+//i++
+// Detect clicks on the button
+
+if (btn) {
+  btn.onclick = function() {
+    console.log("clicked")
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn.classList.toggle("dipped");
+    //IDK WHAT THIS DOES T_T
+  }
+}
+
+else if (btn2) {
+  btn2.onclick = function() {
+    console.log("clicked")
+    // The JS works in conjunction with the 'dipped' code in style.css
+    btn2.classList.toggle("dipped");
+    //IDK WHAT THIS DOES T_T
+  }
+}
+
+
+// This is a single line JS comment
+/*
+This is a comment that can span multiple lines 
+- use comments to make your own notes!
+*/
